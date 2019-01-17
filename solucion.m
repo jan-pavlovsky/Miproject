39b3254cc63f735dc36c6483b4c40897f0300e2f
JanPavlovsky <jan@pavlovsky.net>
2019-01-17 17:27:17 +0100

    count by day mapreducer

--- solucion.m
+++ solucion.m
@@ -5,6 +5,8 @@ for filename = files
     filenames = [filenames, filename.name];
 end
 
+filenames = "2000.csv";
+
 ds = datastore(filenames,  'TreatAsMissing', 'NA');
 
 ds.SelectedFormats{strcmp(ds.SelectedVariableNames, 'UniqueCarrier')} = '%s';
@@ -14,13 +16,31 @@ ds.SelectedFormats{strcmp(ds.SelectedVariableNames, 'Dest')} = '%s';
 ds.SelectedFormats{strcmp(ds.SelectedVariableNames, 'CancellationCode')} = '%s';
 %ds.SelectedVariableNames = {'CancellationCode', 'Cancelled', 'Origin'};
 
-res = mapreduce(ds, @mapper, @reducer);
 
 
-function mapper (data, ~, intermKVStore)
+res = mapreduce(ds, @plotMapper, @plotReducer);
+
+arr = zeros(366*20);
+
+function plotMapper (data, info, intermKVStore)
+    dts = datetime(data.Year, data.Month, data.DayofMonth);
+    dayStrings = yyyymmdd(dts);
+    dayStringsCat = categorical(dayStrings);
     
+    keys = categories(dayStringsCat);
+    counts = num2cell(countcats(dayStringsCat));
+    
+    %dayOfYear = day(dt, 'dayofyear');
+    %totalDay = dayOfYear + (data.Year - 1987 * 365);
+    
+    addmulti(intermKVStore, keys, counts);
 end
 
-function reducer (intermKey, intermValIter, outKVStore)
-
+function plotReducer (intermKey, intermValIter, outKVStore)
+    numberOfFlights = 0;
+    while hasnext(intermValIter)
+        nextCount = getnext(intermValIter);
+        numberOfFlights = numberOfFlights + nextCount;
+    end
+    add(outKVStore, intermKey, numberOfFlights);
 end
