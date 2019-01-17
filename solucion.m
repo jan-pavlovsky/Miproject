e07b797d8d8949945df86fbb13ac6653f86e2e98
JanPavlovsky <jan@pavlovsky.net>
2019-01-17 16:02:05 +0100

    string colums

--- solucion.m
+++ solucion.m
@@ -6,15 +6,19 @@ for filename = files
 end
 
 ds = datastore(filenames,  'TreatAsMissing', 'NA');
+
+ds.SelectedFormats{strcmp(ds.SelectedVariableNames, 'UniqueCarrier')} = '%s';
 ds.SelectedFormats{strcmp(ds.SelectedVariableNames, 'TailNum')} = '%s';
+ds.SelectedFormats{strcmp(ds.SelectedVariableNames, 'Origin')} = '%s';
+ds.SelectedFormats{strcmp(ds.SelectedVariableNames, 'Dest')} = '%s';
 ds.SelectedFormats{strcmp(ds.SelectedVariableNames, 'CancellationCode')} = '%s';
-ds.SelectedVariableNames = {'CancellationCode', 'Cancelled', 'Origin'};
+%ds.SelectedVariableNames = {'CancellationCode', 'Cancelled', 'Origin'};
 
 res = mapreduce(ds, @mapper, @reducer);
 
 
 function mapper (data, ~, intermKVStore)
-
+    
 end
 
 function reducer (intermKey, intermValIter, outKVStore)
